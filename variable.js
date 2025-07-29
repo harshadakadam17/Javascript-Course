63b7db65d4ec7b7f3a44dd684b41b808efb0d2ba
Harshada Kadam <harshadakadamjob@gmail.com>
2025-07-29 16:17:55 +0530

    Variables in javascript

---
+++ variable.js
@@ -0,0 +1,12 @@
+let myname="Harshada"; //string
+let age=22; // Number
+let isStudent=true; // Boolean
+let score; // Undefined
+let nothing=null; // Null
+
+
+console.log(myname);
+console.log(age);
+console.log(isStudent);
+console.log(score);
+console.log(nothing);
