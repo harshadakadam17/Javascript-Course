e69aaf79477cd0a2015c3acfadbdbf92ab434060
Harshada Kadam <harshadakadamjob@gmail.com>
2025-08-03 15:01:31 +0530

    Conditional Statments in javascript

---
+++ Conditional_statment.js
@@ -0,0 +1,53 @@
+// If Condition
+
+/*
+let a = 19;
+
+if (a >= 18) {
+  console.log("You can vote");
+}
+
+*/
+
+
+
+
+//If-else Condition
+
+let a = 16;
+
+if (a >= 18) {
+  console.log("you can vote");
+} else {
+  console.log("you can not vote");
+}
+
+
+//if...else if...else Ladder
+
+let marks = 75;
+
+if (marks >= 90) {
+  console.log("Grade: A");
+} else if (marks >= 70) {
+  console.log("Grade: B");
+} else {
+  console.log("Grade: C");
+}
+
+
+
+//switch Statement
+
+let day = "Monday";
+
+switch (day) {
+  case "Monday":
+    console.log("Start of the week");
+    break;
+  case "Friday":
+    console.log("Weekend is near");
+    break;
+  default:
+    console.log("Another day");
+}
