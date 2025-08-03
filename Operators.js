c86c0b94def99e8851c9634a28a226b1500d16a4
Harshada Kadam <harshadakadamjob@gmail.com>
2025-08-03 14:54:01 +0530

    Operators in javascript

---
+++ Operators.js
@@ -0,0 +1,7 @@
+let num1 = 8;
+let num2 = 4;
+
+console.log("Add:", num1 + num2);
+console.log("Equal:", num1 == num2);
+console.log("Strict Equal:", num1 === num2);
+console.log("Logical AND:", num1 > 5 && num2 < 5);
